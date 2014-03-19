66ef4886289d512670a2ece7e4c6aa3af190d117
Juan Caraballo <juan.caraballo17@gmail.com>
2014-03-19 00:50:18 -0600

    -Completed map interface with clustering and faceted navigation integration. -Added url parameters parsing and management. -Added dataseries table.

--- src/webtsainterface/static/js/tsascript.js
+++ src/webtsainterface/static/js/tsascript.js
@@ -1,25 +1,62 @@
-var TsaApplication = TsaApplication || {};
+var TsaApplication = (function(self){
+    self.initialParameters = {};
 
-TsaApplication.bindEvents = function() {
-    document.addEventListener("facetsloaded", function()  {
-        TsaApplication.UiHelper.renderFacets($("#leftPanel"));
-    });
+    self.initializeApplication = function() {
 
-    document.addEventListener("dataloaded", function() {
-        TsaApplication.UiHelper.renderFilterItems();
-    });
+        self.initialParameters = getUrlParameters();
+        var selectedView = self.initialParameters['view'] || 'map';
+        self.UiHelper.loadView(selectedView);
 
-    document.addEventListener('datafiltered', function(event) {
-        //update map markers and dataseries table.
-        TsaApplication.UiHelper.updateSeriesCount();
-    });
-};
+        bindEvents();
+        self.DataManager.loadData();
+    };
+
+
+    function getUrlParameters() {
+        var search = location.search.substring(1);
+        return (search)? JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g,'":"') + '"}',
+                            function(key, value) { return key===""?value:decodeURIComponent(value) }):{};
+    }
+
+    function bindEvents() {
+
+       document.addEventListener("facetsloaded", function()  {
+           self.UiHelper.renderFacets($("#leftPanel"));
+       });
+
+       document.addEventListener("dataloaded", function() {
+           self.UiHelper.renderFilterItems();
+           self.TableController.initializeTable();
+       });
+
+       document.addEventListener('datafiltered', function() {
+           //update map markers and dataseries table.
+           self.UiHelper.updateSeriesCount();
+           self.MapController.updateSitesMarkers();
+       });
+
+       document.addEventListener('sitesloaded', function() {
+           self.MapController.loadMarkers();
+       });
+
+       google.maps.event.addDomListener(window, "load", function() {
+           self.MapController.initializeMap();
+           google.maps.event.trigger(self.MapController.map, 'resize');
+       });
+
+       $('a[href="#mapContent"]').on('shown', function() {
+           google.maps.event.trigger(self.MapController.map, 'resize');
+       });
+    }
+
+    return self;
+}(TsaApplication || {}));
 
 /**
  *  Data Manager
  */
 TsaApplication.DataManager = (function (self) {
-    var dataLoader = { loadedData: 0, dataToLoad: ['facets', 'dataseries', 'sites'] }
+    var dataLoader = { loadedData: 0, dataToLoad: ['facets', 'dataseries', 'sites'] };
 
     //data
     self.dataseries = [];
@@ -108,7 +145,6 @@ TsaApplication.DataManager = (function (self) {
             facet.updateFacetSeries();
         });
     }
-
 	return self;
 }(TsaApplication.DataManager || {}));
 
@@ -118,26 +154,173 @@ TsaApplication.DataManager = (function (self) {
  */
 TsaApplication.MapController = (function (self) {
     self.map = {};
-    self.markers = [];
     var settings = {};
-
-    function loadMarkers() {
-
-    }
+    var markersManagers = {};
 
     self.initializeMap = function() {
         settings = {
-            center: new google.maps.LatLng(-34.397, 150.644),
-            zoom: 8
+            center: new google.maps.LatLng(41.0648701, -111.4622151),
+            mapTypeId: google.maps.MapTypeId.TERRAIN,
+            zoom: 7
         };
-        
-        self.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-        loadMarkers();
+        self.map = new google.maps.Map(document.getElementById("map_canvas"), settings);
     };
+
+    self.loadMarkers = function() {
+        loadMarkerManagers();
+        TsaApplication.DataManager.sites.forEach(function(site) {
+            var marker = createMarker(site);
+            markersManagers[site.sourcedataserviceid].addMarker(marker);
+            var markerInfoWindow = new google.maps.InfoWindow({ content: TsaApplication.UiHelper.siteInfoWindowTemplate({
+                sitecode: site.sitecode, sitename: site.sitename, network: site.network, sitetype: site.sitetype,
+                latitude: site.latitude, longitude: site.longitude, state: site.state, county: site.county})
+            });
+            google.maps.event.addListener(marker, 'click', function() { markerInfoWindow.open(self.map, marker); });
+        });
+    };
+
+    self.updateSitesMarkers = function() {
+        for (var property in markersManagers) {
+            if (markersManagers.hasOwnProperty(property)) {
+                var markersManager = markersManagers[property];
+                markersManager.getMarkers().forEach(function(marker) {
+                    var siteMarker = _.find(TsaApplication.Search.filteredSites, function(site) {
+                        return site.sitename === marker.title;
+                    });
+                    marker.setVisible(( (siteMarker)? true: false ));
+                });
+                markersManager.repaint();
+            }
+        }
+    };
+
+    function loadMarkerManagers() {
+        var uniqueNetworks = _.uniq(TsaApplication.DataManager.sites, function(site) { return site.sourcedataserviceid; });
+        var networkIds = _.pluck(uniqueNetworks, 'sourcedataserviceid');
+        var networkNames = _.pluck(uniqueNetworks, 'network');
+        var networks = _.object(networkIds, networkNames);
+
+        networkIds.forEach(function(networkId) {
+            var networkMarkersManager = new MarkerClusterer(self.map);
+            var cssClass = 'network' + networkId;
+            networkMarkersManager.setIgnoreHidden(true);
+            networkMarkersManager.setMinimumClusterSize(4);
+            networkMarkersManager.setClusterClass("network-cluster " + cssClass);
+            networkMarkersManager.setStyles([{
+                url: '', width: 30, height: 30, textSize: 12, anchorText: [0, -1],
+                fontFamily: '"HelveticaNeue-Light", "Helvetica Neue Light",\
+                    "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif'
+            }]);
+            networkMarkersManager.setCalculator(function (markers) {
+              return { text: markers.length.toString(), index: 1, title: networks[networkId] };
+            });
+            TsaApplication.UiHelper.addColorToClass(cssClass, getMarkerColorMapping(networkId));
+
+            markersManagers[networkId] = networkMarkersManager;
+        });
+    }
+
+    function createMarker(site) {
+        var color = getMarkerColorMapping(site.sourcedataserviceid).hex;
+        var pinImage = new google.maps.MarkerImage('https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=%20|' + color);
+        var marker = new google.maps.Marker({
+            title: site.sitename,
+            position: new google.maps.LatLng(site.latitude, site.longitude),
+            icon: pinImage
+        });
+        google.maps.event.addDomListener(marker, "visible_changed", function() {
+            if (marker.getVisible()) {
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+                setTimeout(function(){ marker.setAnimation(null); }, 2000);
+            }
+        });
+
+        return marker;
+    }
+
+    function getMarkerColorMapping(number) {
+        var red = Math.floor(Math.exp(number) * 255 * ((Math.sin(number) + 2))) % 255;
+        var green = Math.floor(Math.exp(number) * 255 * ((Math.cos(number) + 2))) % 255;
+        var blue = Math.floor(Math.exp(number) * 255 * ((Math.sin(2 * number) + 2))) % 255;
+        return {
+            rgb: { red: red, green: green, blue: blue },
+            hex: ((1 << 24) + (red << 16) + (green << 8) + blue).toString(16).slice(1)
+        };
+    }
 
 	return self;
 }(TsaApplication.MapController || {}));
 
+
+TsaApplication.TableController = (function(self) {
+    self.dataseriesTable = {};
+
+    self.initializeTable = function() {
+        self.dataseriesTable = $('#datasetsTable').DataTable({
+            "sDom": 'RCfrtiS',
+            "sScrollY": "350px",
+            "sScrollX": "100%",
+            "bScrollCollapse": true,
+            "bProcessing": true,
+            "bFilter": true,
+            "bDeferRender": true,
+            "bAutoWidth": true,
+            "bStateSave": true,
+            "oColReorder": {
+                "iFixedColumns": 1
+            },
+            "oColVis": {
+                "aiExclude": [0, 1]
+            },
+            "aaData": TsaApplication.DataManager.dataseries,
+            "aoColumns": [
+                {
+                    "mDataProp": 'seriesid',
+                    "bSearchable": false,
+                    "bSortable": false,
+                    "sType": "html",
+                    "mRender": function ( data, type ) {
+                        return '<input type="checkbox" data-seriesid="' + data + '">';
+                    }
+                },
+                { "sTitle": "Series",  "mDataProp": 'seriesid' },
+                { "sTitle": "Network",  "mDataProp": 'network' },
+                { "sTitle": "Site Code", "mDataProp": 'sitecode' },
+                { "sTitle": "Site Name", "mDataProp": 'sitename', "bVisible": false },
+                { "sTitle": "Latitude", "mDataProp": 'latitude', "bVisible": false },
+                { "sTitle": "Longitude", "mDataProp": 'longitude', "bVisible": false },
+                { "sTitle": "State", "mDataProp": 'state', "bVisible": false },
+                { "sTitle": "County", "mDataProp": 'county', "bVisible": false },
+                { "sTitle": "Site Type", "mDataProp": 'sitetype', "bVisible": false },
+                { "sTitle": "Variable Code", "mDataProp": 'variablecode', "bVisible": false },
+                { "sTitle": "Variable Name", "mDataProp": 'variablename' },
+                { "sTitle": "Method", "mDataProp": 'methoddescription', "bVisible": false },
+                { "sTitle": "Units", "mDataProp": 'variableunitsname', "bVisible": false },
+                { "sTitle": "Units Type", "mDataProp": 'variableunitstype', "bVisible": false },
+                { "sTitle": "Units Abreviation", "mDataProp": 'variableunitsabbreviation', "bVisible": false },
+                { "sTitle": "Sample Medium", "mDataProp": 'samplemedium', "bVisible": false },
+                { "sTitle": "Value Type", "mDataProp": 'valuetype', "bVisible": false },
+                { "sTitle": "Data Type", "mDataProp": 'datatype', "bVisible": false },
+                { "sTitle": "Category", "mDataProp": 'generalcategory', "bVisible": false },
+                { "sTitle": "Time Support", "mDataProp": 'timesupport', "bVisible": false },
+                { "sTitle": "Time Support Units Name", "mDataProp": 'timesupportunitsname', "bVisible": false },
+                { "sTitle": "Time Support Units Type", "mDataProp": 'timesupportunitstype', "bVisible": false },
+                { "sTitle": "Time Support Units Abbreviation", "mDataProp": 'timesupportunitsabbreviation', "bVisible": false },
+                { "sTitle": "Quality Control Level", "mDataProp": 'qualitycontrolleveldefinition', "bVisible": false },
+                { "sTitle": "Source Organization", "mDataProp": 'sourceorganization', "bVisible": false },
+                { "sTitle": "Source Description", "mDataProp": 'souredescription', "bVisible": false },
+                { "sTitle": "Begin DateTime", "mDataProp": 'begindatetime', "bVisible": false },
+                { "sTitle": "End DateTime", "mDataProp": 'enddatetime', "bVisible": false },
+                { "sTitle": "UTC Offset", "mDataProp": 'utcoffset', "bVisible": false },
+                { "sTitle": "Number Observations", "mDataProp": 'numberobservations' },
+                { "sTitle": "Date Last Updated", "mDataProp": 'datelastupdated' },
+                { "sTitle": "Active", "mDataProp": 'isactive' }
+            ]
+        });
+    };
+
+    return self;
+}(TsaApplication.TableController || {}));
 
 /**
  *  Visualization Manager
@@ -210,11 +393,24 @@ TsaApplication.Search = (function (self) {
  *  Ui Handler
  */
 TsaApplication.UiHelper = (function (self) {
+    var visibleViewClass = 'active';
+    var defaultTabElementId = '#mapTab';
+    var defaultContentElementId = '#mapContent';
+
+    self.loadView = function(view) {
+        var tabElement = $('[id="' + (view + 'Tab') + '"');
+        var contentElement = $('[id="' + (view + 'Content') + '"');
+        tabElement = (tabElement.length == 0)? $(defaultTabElementId): tabElement;
+        contentElement = (contentElement.length == 0)? $(defaultContentElementId): contentElement;
+
+        tabElement.addClass(visibleViewClass);
+        contentElement.addClass(visibleViewClass);
+    };
 
     self.facetsTemplate = _.template("<div class='panel panel-default'>\
         <div class='panel-heading'>\
             <h4 class='panel-title'>\
-                <a data-toggle='collapse' class='accordion-toggle' data-parent='#accordion' href='#<%= facetid %>'> \
+                <a data-toggle='collapse' class='accordion-toggle' data-parent='#accordion' href='#<%= facetid %>'>\
                     <%= facettitle %> \
                 </a>\
             </h4>\
@@ -235,6 +431,23 @@ TsaApplication.UiHelper = (function (self) {
             <input type='checkbox' data-facet='<%= facet %>' value='<%= id %>'><%= name %>\
         </label>\
     </li>");
+
+    self.siteInfoWindowTemplate = _.template("<section class='site-infowindow'>\
+        <header><h5><%= sitename %></h5> <h6><%= sitecode %></h6></header>\
+        <div class='siteDetails'>\
+            <div class='column'>\
+                <span class='detailItem'><span>Network:</span> <%= network %></span>\
+                <span class='detailItem'><span>State:</span> <%= state %></span>\
+                <span class='detailItem'><span>Site Type:</span> <%= sitetype %></span>\
+            </div>\
+            <div class='column'>\
+                <span class='detailItem'><span>County:</span> <%= county %></span>\
+                <span class='detailItem'><span>Latitude:</span> <%= latitude %></span>\
+                <span class='detailItem'><span>Longitude:</span> <%= longitude %></span>\
+            </div>\
+            <div class='clearfix'></div>\
+        </div>\
+    </section>");
 
     self.renderFacets = function(parent){
         var facets = [];
@@ -278,16 +491,31 @@ TsaApplication.UiHelper = (function (self) {
         });
     };
 
+    self.updateTabsFilteredCount = function() {
+        var sitesCount = TsaApplication.Search.filteredSites.length;
+        var seriesCount = TsaApplication.Search.filteredDataseries.length;
+        $("#datasetsTab .badge").text(seriesCount);
+        $("#mapTab .badge").text(sitesCount);
+    }
 
+    self.addColorToClass = function(cssClass, color) {
+        var rgbaString = 'rgba(' + color.rgb.red + ', ' + color.rgb.green + ', ' + color.rgb.blue + ', 0.86)';
+        var backgroundColorString = 'background-color: ' + rgbaString;
+        $('<style>.' + cssClass + ' { ' + backgroundColorString + '; }</style>').appendTo('head');
+    }
 
 	return self;
 }(TsaApplication.UiHelper || {}));
 
 
+
 $(document).ready(function(){
-    TsaApplication.bindEvents();
-    TsaApplication.DataManager.loadData();
+    TsaApplication.initializeApplication();
 });
+
+
+
+
 
 /*
  * object.watch polyfill
