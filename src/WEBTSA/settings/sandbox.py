fbc796c09d1e2185161dffb80a319686119bb687
unknown <juan.caraballo17@gmail.com>
2014-05-31 00:12:01 -0600

    Added sandbox configuration for error custom logging.

--- src/WEBTSA/settings/sandbox.py
+++ src/WEBTSA/settings/sandbox.py
@@ -1,4 +1,8 @@
+import sys
 from WEBTSA.settings.base import *
+
+#For error logging (helicon zoo error trace logging doesn't work)
+#sys.stderr = open('err.log', 'w')
 
 DATABASE_PATH = os.path.join(BASE_DIR, os.pardir, 'Internal')
 DATABASES['default']['NAME'] = DATABASE_PATH;
@@ -11,3 +15,4 @@ SITE_URL = os.environ['APPL_VIRTUAL_PATH'] + "/"
 
 STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
 STATIC_URL = SITE_URL + 'static/'
+
