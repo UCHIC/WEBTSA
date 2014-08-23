c5a1804fef8c13b3cd2b8524ee44c38fb0631561
Juan Caraballo <juan.caraballo17@gmail.com>
2014-08-23 00:50:04 -0600

    Updated production server configuration.

--- src/WEBTSA/settings/production.py
+++ src/WEBTSA/settings/production.py
@@ -1,1 +1,18 @@
+import sys
 from WEBTSA.settings.base import *
+
+#For error logging (helicon zoo error trace logging doesn't work)
+#sys.stderr = open('err.log', 'w')
+
+DATABASE_PATH = os.path.join(BASE_DIR, os.pardir, 'Internal')
+DATABASES['default']['NAME'] = DATABASE_PATH;
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+SITE_ROOT = os.environ['APPL_PHYSICAL_PATH']
+SITE_URL = os.environ['APPL_VIRTUAL_PATH'] + "/"
+
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
+STATIC_URL = SITE_URL + 'static/'
+
